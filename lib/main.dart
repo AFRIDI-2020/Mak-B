c0d64f7e0d9b349c2273c484c5b14c51e1ed691b
Sujit Sarkar <snigdho532@gmail.com>
2021-09-20 14:31:31 +0600

    4th commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mak_b/pages/login_page.dart';
+import 'package:mak_b/pages/product_page.dart';
 import 'package:mak_b/variables/theme.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Mak B',
       theme: theme(),
-      home: LoginPage(),
+      home: ProductPage(),
     );
   }
 }
